b541e162a9d62fd76e18ed1ff7bc882e342408ba
Joshua Cole <j_cole_356@yahoo.com>
2015-06-27 21:39:10 -0700

    refactor board class to seperate file

--- js/si_game.js
+++ js/si_game.js
@@ -3,7 +3,7 @@
     window.SI = {};
   }
 
-  // I don't think I'm even using this anymore
+  // Use this to check if alien is hit.
   Array.prototype.equals = function (array) {
     if (this.length !== array.length) {
       return false;
@@ -17,7 +17,6 @@
     return true;
   };
 
-  // May need to make use of the coord class like in snake
   var Alien = SI.Alien = function(coord, board) {
     this.coord = coord;
     this.board = board;
@@ -31,87 +30,87 @@
     this.coord[1] = this.coord[1] + (1 * this.board.dir);
   };
 
-  var Board = SI.Board = function(width, height) {
-    this.width = width;
-    this.height = height;
-    this.dir = -1;
-    this.topAlienRow = 0;
-    this.ship = new Ship([(this.height - 1), 0], this);
-    this.aliens = [];
-    this.lasers = [];
-    //Initalize the aliens
-    for (var i = 0; i < 5; i++) {
-      for (var j = 6; j < 18; j++) {
-        var alien = new Alien([i, j], this);
-        this.aliens.push(alien);
-      }
-    }
-  };
-
-  Board.prototype.alienAtEdge = function () {
-    var atEdge = false;
-    this.aliens.forEach(function(alien) {
-      if (alien.coord[1] === 0 || alien.coord[1] === (this.width - 1)) {
-        atEdge = true;
-      }
-    }.bind(this));
-    return atEdge;
-  };
-
-  // Need to set the initial state of the invaders
-  Board.prototype.blankGrid = function () {
-    var grid = [];
-    for (var i = 0; i < this.height; i++) {
-      var row = [];
-      for (var j = 0; j < this.width; j++) {
-        row.push("");
-      }
-      grid.push(row);
-    }
-
-    return grid;
-  };
-
-  // Evaluates win or loss
-  Board.prototype.gameOver = function () {
-    var gameState = null;
-    if (this.aliens.length === 0) {
-      gameState = "win";
-    }
-    // Not sure why this stopped working.
-    this.aliens.forEach(function(alien) {
-      if (alien.coord[0] === this.height - 1) {
-        gameState = "lose";
-      }
-    }.bind(this));
-
-    return gameState;
-  };
-
-  Board.prototype.isOccupied = function (coord) {
-    var alienAtLocation = null;
-    this.aliens.forEach(function(alien) {
-      if (alien.coord.equals(coord)) {
-        alienAtLocation = alien;
-      }
-    });
-
-    return alienAtLocation;
-  };
-
-  // Removes the object from the board.
-  Board.prototype.remove = function (object) {
-    if (object instanceof SI.Laser) {
-      this.lasers.splice(this.lasers.indexOf(object), 1);
-    } else if (object instanceof SI.Alien) {
-      this.aliens.splice(this.aliens.indexOf(object), 1);
-    }
-  };
-
-  Board.prototype.toggleDirection = function () {
-    this.dir = this.dir * -1;
-  };
-
+  // var Board = SI.Board = function(width, height) {
+  //   this.width = width;
+  //   this.height = height;
+  //   this.dir = -1;
+  //   this.topAlienRow = 0;
+  //   this.ship = new Ship([(this.height - 1), 0], this);
+  //   this.aliens = [];
+  //   this.lasers = [];
+  //   //Initalize the aliens
+  //   for (var i = 0; i < 5; i++) {
+  //     for (var j = 6; j < 18; j++) {
+  //       var alien = new Alien([i, j], this);
+  //       this.aliens.push(alien);
+  //     }
+  //   }
+  // };
+  //
+  // Board.prototype.alienAtEdge = function () {
+  //   var atEdge = false;
+  //   this.aliens.forEach(function(alien) {
+  //     if (alien.coord[1] === 0 || alien.coord[1] === (this.width - 1)) {
+  //       atEdge = true;
+  //     }
+  //   }.bind(this));
+  //   return atEdge;
+  // };
+  //
+  // // Need to set the initial state of the invaders
+  // Board.prototype.blankGrid = function () {
+  //   var grid = [];
+  //   for (var i = 0; i < this.height; i++) {
+  //     var row = [];
+  //     for (var j = 0; j < this.width; j++) {
+  //       row.push("");
+  //     }
+  //     grid.push(row);
+  //   }
+  //
+  //   return grid;
+  // };
+  //
+  // // Evaluates win or loss
+  // Board.prototype.gameOver = function () {
+  //   var gameState = null;
+  //   if (this.aliens.length === 0) {
+  //     gameState = "win";
+  //   }
+  //   // Not sure why this stopped working.
+  //   this.aliens.forEach(function(alien) {
+  //     if (alien.coord[0] === this.height - 1) {
+  //       gameState = "lose";
+  //     }
+  //   }.bind(this));
+  //
+  //   return gameState;
+  // };
+  //
+  // Board.prototype.isOccupied = function (coord) {
+  //   var alienAtLocation = null;
+  //   this.aliens.forEach(function(alien) {
+  //     if (alien.coord.equals(coord)) {
+  //       alienAtLocation = alien;
+  //     }
+  //   });
+  //
+  //   return alienAtLocation;
+  // };
+  //
+  // // Removes the object from the board.
+  // Board.prototype.remove = function (object) {
+  //   if (object instanceof SI.Laser) {
+  //     this.lasers.splice(this.lasers.indexOf(object), 1);
+  //   } else if (object instanceof SI.Alien) {
+  //     this.aliens.splice(this.aliens.indexOf(object), 1);
+  //   }
+  // };
+  //
+  // Board.prototype.toggleDirection = function () {
+  //   this.dir = this.dir * -1;
+  // };
+  //
   var Laser = SI.Laser = function (coord, board) {
     this.coord = coord;
     this.board = board;
