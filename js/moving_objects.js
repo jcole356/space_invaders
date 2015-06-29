3d1cc50b75d985740d7da9ec9fa7867cc6395157
Joshua Cole <j_cole_356@yahoo.com>
2015-06-28 21:36:08 -0700

    alien laser starts from exact location of alien

--- js/moving_objects.js
+++ js/moving_objects.js
@@ -37,7 +37,8 @@
 
   // Need to be able to shoot
   Alien.prototype.shoot = function () {
-    var laserCoord = [(this.coord[0] + 1), this.coord[1]];
+    // Change this to start at the same location as the alien
+    var laserCoord = [(this.coord[0]), this.coord[1]];
     var laser = new AlienLaser(laserCoord, this.board);
     this.board.alienLasers.push(laser);
     window.view.render();
